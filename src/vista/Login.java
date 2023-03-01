bc0a47bdbfb7a6dbf97c2f43657581c3b60dcead
mark-1701 <vini.muralles@gmail.com>
2023-03-01 00:36:50 -0600

    corecciones 7

--- src/vista/Login.java
+++ src/vista/Login.java
@@ -90,6 +90,9 @@ public class Login extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        /*Para ingresar al sistema ventas, debera de entrar con un nombre 
+        y contrasena ya registrado*/
+        
         usuario = txtUser.getText();
         password = txtPass.getText();
         if (usuario.equals("admin") && password.equals("admin")) {
